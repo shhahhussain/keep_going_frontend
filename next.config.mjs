a391c9d434b39adfc7b5943c258b501be78dfbfe
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2025-07-22 00:30:41 +0500

    support custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
+const isCustomDomain = process.env.NEXT_PUBLIC_CUSTOM_DOMAIN === "true";
 
 export default {
   output: "export",
   reactStrictMode: true,
-  basePath: isProd ? "/keep_going_frontend" : "",
-  assetPrefix: isProd ? "/keep_going_frontend/" : "",
+  basePath: isProd && !isCustomDomain ? "/keep_going_frontend" : "",
+  assetPrefix: isProd && !isCustomDomain ? "/keep_going_frontend/" : "",
   images: { unoptimized: true },
 };
