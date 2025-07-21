4277de4f1717e6a83c8ac06b874fa4b0e5399bb2
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2025-07-21 23:46:47 +0500

    Updates

---
+++ _next/static/chunks/app/layout-2b67b144fb0f2b76.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2129:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,9324,23)),Promise.resolve().then(a.t.bind(a,9399,23))},9324:()=>{},9399:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_cbfae4"}}},e=>{var s=s=>e(e.s=s);e.O(0,[110,441,684,358],()=>s(2129)),_N_E=e.O()}]);
